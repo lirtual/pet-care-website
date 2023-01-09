caa3ef0c3f4c9b6e043add6f8433673dc3203dd3
xAkashax <kbilan1992@gmail.com>
2023-01-09 22:30:26 +0100

    finishing html and css for sections

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 'use strict';
+
+// Page Navigation
+
+// Scrolling (Button)
+
+// Sticky Navigation
+
+// Lazy loading images
+
+// Menu fade animation
+
+// Reveal Sections
+
+// Cookies
+
+// Modal Window
+
+// Tabbed component
+
+// SLider
+
+// Event Handlers
